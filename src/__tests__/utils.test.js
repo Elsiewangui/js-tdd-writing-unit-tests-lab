25d87c5c6b698aafbf1375f13edfc9f24239597e
Wangui <elswangui@gmail.com>
2024-08-07 19:57:53 +0300

    update utils.js

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,20 @@
 // Your tests here
+import isPalindrome from "../utils";
+
+describe("isPalindrome", () => {
+    it("returns true for a palindrome word", () => {
+      const word = "racecar";
+  
+      const result = isPalindrome(word);
+  
+      expect(result).toBe(true);
+    });
+  
+    it("returns false for a non-palindrome word", () => {
+      const word = "hello";
+  
+      const result = isPalindrome(word);
+  
+      expect(result).toBe(false);
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+function isPalindrome(str){
+    const cleanedStr = str.toLowerCase().replace();
+  return cleanedStr === cleanedStr.split('').reverse().join('');
+}
+
+export default isPalindrome;
+    
